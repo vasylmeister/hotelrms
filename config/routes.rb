4740f89d8f5b39fee1340327b9cb17d2b3fc3550
Vasyl <vasylmeister@gmail.com>
2016-12-14 19:55:01 +0000

    Create dashboard page, add basic grid and styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'dashboard' => 'static_pages#dashboard_view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
