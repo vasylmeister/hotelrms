b19ef3afeaf9b8bc14f4d561ede1710cd6a8ec2c
Vasyl <vasylmeister@gmail.com>
2017-01-31 20:19:23 +0000

    recovered files to the point I need, reset DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125134707) do
+ActiveRecord::Schema.define(version: 20170128200434) do
+
+  create_table "bed_types", force: :cascade do |t|
+    t.string "name"
+    t.float  "width"
+  end
 
   create_table "private_beds", force: :cascade do |t|
     t.integer "room_id"
